9acf1d3a93a1666474bcc38e906253ac36d37bd1
Giancarlo Zuliani <slaigox@gmail.com>
2020-10-27 17:33:53 +0100

    exercise complete

--- script.js
+++ script.js
@@ -18,25 +18,26 @@ console.log(isPalindroma(name))
 function run(){
   var userChoice = prompt('pari o dispari')
   if(userChoice == 'pari' || userChoice == 'dispari'){
-        var sum = gameset()
-        if((userChoice == 'pari' && isEven(sum)) ||(userChoice == 'dispari' && !isEven(sum))){
-            console.log('hai vinto');
-        }else{
-          console.log('hai perso')
-        }
+      var sum = gameset()
+      if((userChoice == 'pari' && isEven(sum)) ||(userChoice == 'dispari' && !isEven(sum))){
+          console.log('hai vinto');
       }else{
-        run();
+      console.log('hai perso')
       }
-
+    }else{
+    run();
+  }
 }
 
 function gameset(){
-  var userNum = prompt('inserisci un numero da 1 a 5');
+  var userNum =parseInt(prompt('inserisci un numero da 1 a 5'));
   if(userNum < 0 || userNum > 5){
     gameset();
 }else{
-  var cpuNum = Math.floor(Math.random() * 6)
+  var cpuNum = Math.floor((Math.random() * 6) + 1)
+  console.log(cpuNum)
   var sum = cpuNum + userNum;
+    console.log(sum)
     return sum;
   }
 };
