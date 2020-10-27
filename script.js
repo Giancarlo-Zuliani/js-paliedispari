ae7e1a6e565ab0ffed4d1194cb02d0e7d38dcf6a
Giancarlo Zuliani <slaigox@gmail.com>
2020-10-27 17:28:45 +0100

    function added

--- script.js
+++ script.js
@@ -14,3 +14,41 @@ function isPalindroma(string){
 }
 
 console.log(isPalindroma(name))
+
+function run(){
+  var userChoice = prompt('pari o dispari')
+  if(userChoice == 'pari' || userChoice == 'dispari'){
+        var sum = gameset()
+        if((userChoice == 'pari' && isEven(sum)) ||(userChoice == 'dispari' && !isEven(sum))){
+            console.log('hai vinto');
+        }else{
+          console.log('hai perso')
+        }
+      }else{
+        run();
+      }
+
+}
+
+function gameset(){
+  var userNum = prompt('inserisci un numero da 1 a 5');
+  if(userNum < 0 || userNum > 5){
+    gameset();
+}else{
+  var cpuNum = Math.floor(Math.random() * 6)
+  var sum = cpuNum + userNum;
+    return sum;
+  }
+};
+
+
+function isEven(x){
+  if (x % 2 == 0){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+run();
